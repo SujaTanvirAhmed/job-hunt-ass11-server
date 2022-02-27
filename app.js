fe3c381a49bb094bb819c635f42356055b3070b7
Suja Tanvir Ahmed <30548517+SujaTanvirAhmed@users.noreply.github.com>
2022-02-27 09:43:46 +0600

    prepared for heroku deploy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const cors = require("cors");
 
+// Express Server
 const app = express();
 
 app.use(cors());
@@ -12,37 +13,52 @@ const port = process.env.PORT || 5000;
 
 // Mongodb starts
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db(process.env.DB_NAME).collection("users");
-    // perform actions on the collection object
-    console.log("Connected to mongodb");
-    client.close();
-});
+
+async function run() {
+    try {
+        // Dummy data
+        const usersData = [
+            { id: 1001, name: "Suja", prof: "Web Developer" },
+            { id: 1002, name: "Zisad", prof: "Journalist" },
+            { id: 1003, name: "Akash", prof: "Front-Desk Executive" }
+        ];
+        await client.connect();
+        console.log("Mongodb connected!");
+
+        const database = client.db(process.env.DB_NAME);
+        const users = database.collection('users');
+
+        app.get("/", (req, res) => {
+            res.send("job-hunt-ass11-server is running...");
+        });
+
+        app.get("/users", (req, res) => {
+            res.json(usersData);
+        });
+
+        app.post("/users", (req, res) => {
+            const user = req.body;
+            const id = Math.floor(Math.random() * 1000);
+            const userWithId = { id, ...user };
+            console.log(userWithId);
+            res.json(userWithId);
+        });
+
+        app.get("/books", (req, res) => {
+            res.status(200).end("Books are not available now!");
+        });
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
 // Mongodb ends
 
-app.get("/", (req, res) => {
-    res.send("job-hunt-ass11-server is running...");
-});
-
-app.get("/users", (req, res) => {
-    res.json(users);
-});
-
-app.post("/users", (req, res) => {
-    const user = req.body;
-    const id = Math.floor(Math.random() * 1000);
-    const userWithId = { id, ...user };
-    console.log(userWithId);
-    res.json(userWithId);
-});
-
+// Run the Express Server
 app.listen(port, () => {
     console.log(`job-hunt-ass11-server is running on port ${port}.`);
 });
-
-const users = [
-    { id: 1001, name: "Suja", prof: "Web Developer" },
-    { id: 1002, name: "Zisad", prof: "Journalist" },
-    { id: 1003, name: "Akash", prof: "Front-Desk Executive" }
-];
